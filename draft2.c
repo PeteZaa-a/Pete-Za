10630b6442ed6ad87d3efde55edcddba56d73528
PeteZaa-a <68010862@kmitl.ac.th>
2025-10-06 14:37:25 +0700

    Update draft2.c

--- draft2.c
+++ draft2.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-void menu() {
+void menu_list() {
     printf("Student application system\n");
-    printf("=====MENU LISTS=====\n");
-    printf("|   1.Add user     |\n|   2.Edit user    |\n|   3.Update user  |\n|   4.Search user  |\n|   5.Show user    |\n====================\n");
+    printf("======MENU LISTS======\n");
+    printf("|   1.Add user       |\n");
+    printf("|   2.Delete user    |\n");
+    printf("|   3.Update status  |\n");
+    printf("|   4.Search user    |\n");
+    printf("|   5.Show all       |\n");
+    printf("|   6.TESTING 1      |\n");
+    printf("|   7.TESTING 2      |\n");
+    printf("|   8.Exit           |\n"); 
+    printf("======================\n");
+
 }
 
 void add_user() {
     FILE *fptr;
     fptr = fopen("test2.csv","a");
-    char data[80];
-    printf("Enter data to add: ");
-    scanf("%s",&data);
-    fprintf(fptr, "%s\n", data);
-    }
+    char name[15], surname[30], course[15], date[25], status[10], confirm;
+    printf("Enter data to add\n");
+    printf("Enter name & surname: ");
+    scanf("%s %s",&name, &surname);
+    printf("Enter course name: ");
+    scanf("%s", &course);
+    printf("Enter date, Use format = yyyy-mm-dd\n");
+    printf("Enter date:");
+    scanf("%s", &date);
+    printf("Enter status: ");
+    scanf("%s", &status);
 
-void file_check() {
-    FILE *fptr;
-    fptr = fopen("test2.csv", "r");  
-    
-    if (fptr == NULL)
-    {
-        printf("Error");
+    printf("Your data is %s %s %s %s %s\n", name ,surname, course, date, status);
+    fprintf(fptr, "%s %s, %s, %s, %s\n",name, surname, course, date, status);
+       
     }
-    else
-    {
-        printf("Complete");
-    }
-    
-    
-        
-    }
-
-void edit_user() {
-
-}
 
 void show_all() {
     char data[100];
@@ -46,29 +45,34 @@ void show_all() {
 
     while (fgets(data, 100, fptr) != NULL)
     {
-        printf("%s\n",data);
+        printf("%s",data);
     }
     
 }
 
 void search() {
-
-    char user[20];
     FILE *fptr;
     fptr = fopen("test2.csv", "r");
-    printf("Enter user to search: ");
-    scanf("%s", &user);
+    char search[20];
+    printf("Enter to search: ");
+    scanf("%s", search);
+    printf("Searching %s\n",search);
+    char data[250];
+    while (fgets(data, 250, fptr) != NULL)
+    {
+        if (strstr(data, search) != NULL)
+        {
+            printf("%s", data);
+        }
+       
+    }
 
-   char *doing = strstr(fptr,user);
-   if (doing != NULL)
-   {
-    printf("Found");
-   }
-   
     
 }
 
-    
+void update(){
+
+}
 
 int main () {
 
@@ -82,8 +86,9 @@ int main () {
         printf("File created\n");
     }
     
-    menu();
+    menu_list();
 
+    char name[15], surname[30], course[15], date[25], status[10];
     int menu;
     printf("Enter menu: ");
     scanf("%d", &menu);
@@ -92,20 +97,19 @@ int main () {
     case 1:
         printf("Doing add user mode\n");
         add_user() ;
-        file_check();
         break;
     
     case 2:
-        printf("Edit user mode");
+        printf("Delete user mode");
         break;
 
     case 3:
-        printf("Delete user mode");
-        printf("Enter user to delete");
+        printf("Update status mode");
+        printf("Enter user to update status: ");
         break;
 
     case 4:
-        printf("Search\n");
+        printf("Search mode\n");
         search();
 
         break;
@@ -113,6 +117,12 @@ int main () {
     case 5:
         printf("Show all user\n");
         show_all();
+
+        break;
+
+    case 6:
+        printf("File Tester\n");
+
 
         break;
     default:
